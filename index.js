a267dd1ac4c4a6a23c800ba3619f7781e3f80337
hulyak <57311842+hulya95@users.noreply.github.com>
2020-09-05 15:57:28 -0700

    test first interactions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = 3000;
@@ -8,15 +8,17 @@ server.listen(port, () => {
   console.log(`server is listening on port ${server.address().port} `);
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/public/index.html');
+// });
 
 // events : connection and message
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.emit('message', { hulya: 'hey, how are you?' });
-  socket.on('another event', (data) => {
-    console.log(data);
+  socket.on('message', (msg) => {
+    console.log(`message ${msg}`);
+    io.emit('message', msg);
   });
 });
